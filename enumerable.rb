d496da09b75435febbaf748b2cef15239eb1e09c
Karen Siu <k3tang@gmail.com>
2022-06-27 16:09:38 -0700

    edits

--- enumerable.rb
+++ enumerable.rb
@@ -74,20 +74,38 @@ class Array
 
     return new_arr
   end
+
+  def my_zip(*arrs)
+    newarray = []
+    
+    (0...self.length).each do |i|
+      subarray = []
+      subarray << self[i]
+      arrs.my_each do |arr|
+        subarray << arr[i]
+      end 
+      newarray << subarray
+    end 
+    return newarray
+  end 
+
+
+  def my_rotate(num = 1)
+    newarray = []
+    (0...sel)
 end
 
-# a = [1, 2, 3]
-# p a.my_select { |num| num > 1 } # => [2, 3]
-# p a.my_select { |num| num == 4 } # => []
 
-# def my_reject
-# a = [1, 2, 3]
-# p a.my_reject { |num| num > 1 } # => [1]
-# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
-# a = [1, 2, 3]
-# p a.my_any? { |num| num > 1 } # => true
-# p a.my_any? { |num| num == 4 } # => false
-# p a.my_all? { |num| num > 1 } # => false
-# p a.my_all? { |num| num < 4 } # => true
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+
+
